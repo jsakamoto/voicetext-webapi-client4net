2a7686b600655fa5dfe0596be5ec124b4a0713a9
jsakamoto <junichi.jp@gmail.com>
2019-03-19 15:44:51 -0700

    Add Sample .NET Core Console app.

---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using VoiceTextWebAPI.Client;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var client = new VoiceTextClient
+            {
+                APIKey = "{your API key here.}",
+                Speaker = Speaker.Haruka,
+                Emotion = Emotion.Happiness,
+                EmotionLevel = EmotionLevel.High,
+                Volume = 50,
+                Speed = 120,
+                Pitch = 120,
+                Format = Format.WAV
+            };
+            var bytes = await client.GetVoiceAsync("こんにちは。");
+            File.WriteAllBytes(".\\result.wav", bytes);
+        }
+    }
+}
